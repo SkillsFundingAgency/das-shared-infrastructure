48080fe21594edb5ba9320d8053257f27bdf6ecd
Craig Gumbley <craiggumbley@gmail.com>
2019-06-20 16:07:38 +0100

    A new home :fire: :construction_worker:

---
+++ Initialize-SharedInfrastructureDeployment.ps1
@@ -0,0 +1,186 @@
+<#
+.SYNOPSIS
+Shared infrastructure deployment helper
+
+.DESCRIPTION
+Pre deployment script for shared infrastructure deployments. It is designed to be ran in the context of a
+build agent or from a local machine.
+
+Build agent deployments:
+    * This script only prepares the deployment environment
+    * Ensure that the correct template environment variables are present in the release definition
+    * Use the ARM template deployment task
+    * Secret variables must be passed as override parameters to the ARM tepmlate deployment release task
+Local deployments:
+    * ONLY to be used for testing and NOT against any environments that are in service
+    * The script looks for matching environment variables. If they are not present you will be prompted to enter the values
+    * When not in the context of a build server, this script will create the deployment.
+
+.PARAMETER SubscriptionAbbreviation
+Abbreviation of the subscription. It is possible for a subscription to have many environments, for example,
+a development subscription can contain many development environments.
+
+Default: DEV
+
+.PARAMETER EnvironmentNames
+A JSON array of environments to create in a subscription. For example:
+"["AT","TEST""]
+
+Default: $ENV:EnvironmentNames
+
+.PARAMETER Location
+The location of the resources.
+
+Default: West Europe
+
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("DEV", "PP", "PRD", "MO")]
+    [string]$SubscriptionAbbreviation = "DEV",
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("AT", "TEST", "TEST2", "DEMO", "PP", "PRD", "MO")]
+    [string[]]$EnvironmentNames = ($ENV:EnvironmentNames | ConvertFrom-Json),
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("West Europe", "North Europe")]
+    [string]$Location = "West Europe",
+    [Parameter(Mandatory = $false)]
+    [switch]$AcceptDefaults
+)
+
+$TemplateFilePath = "$PSScriptRoot/templates/subscription.template.json"
+$TemplateParametersFilePath = "$PSScriptRoot/templates/subscription.parameters.json"
+
+try {
+    # --- Are We Logged in?
+    $IsLoggedIn = (Get-AzContext -ErrorAction SilentlyContinue).Account
+    if (!$IsLoggedIn) {
+        throw "You are not logged in. Run Add-AzAccount to continue"
+    }
+
+    if (!$EnvironmentNames){
+        throw "No environment names found"
+    }
+
+    # --- Create Resource Groups
+    $ManagementResourceGroupName = "das-$SubscriptionAbbreviation-mgmt-rg".ToLower()
+    $ResourceGroupList = [System.Collections.ArrayList]::new(@($ManagementResourceGroupName))
+    $ResourceGroupList.AddRange(@($EnvironmentNames | ForEach-Object { "das-$_-shared-rg".ToLower() }))
+
+    $ResourceGroupList | ForEach-Object {
+        Write-Host "- Creating Resource Group: $_"
+        $ResourceGroup = Get-AzResourceGroup -Name $_ -Location $Location -ErrorAction SilentlyContinue
+        if (!$ResourceGroup) {
+            $null = New-AzResourceGroup -Name $_ -Location $Location -Confirm:$false
+        }
+    }
+
+    $DatabaseConfiguration = @{ }
+    Write-Host "- Setting up failover group config"
+    # --- Get environment databases for failover group
+    foreach ($Environment in $EnvironmentNames) {
+        $DatabaseConfiguration.Add(
+            $Environment, @{"DatabaseResourceIds" = @() }
+        )
+
+        # --- Get all shared SQL Servers in environment
+        $SqlServerResources = @(Get-AzResource -Name "das-$($Environment.ToLower())-shared-sql*" -ResourceType "Microsoft.Sql/servers")
+
+        # --- If there is more than one then find the primary
+        if ($SqlServerResources.Count -gt 1) {
+            $SqlServer = $SqlServerResources | Where-Object {
+                $FailoverGroup = Get-AzSqlDatabaseFailoverGroup -ServerName $_.Name -ResourceGroupName $_.ResourceGroupName
+                if ($FailoverGroup -and $FailoverGroup.ReplicationRole -eq "Primary") {
+                    return $_
+                }
+            }
+        }
+        elseif ($SqlServerResources -eq 1) {
+            # --- If there is only one, use that
+            $SqlServer = $SqlServerResources[0]
+        }
+        else {
+            continue
+        }
+
+        # --- Get all the databases in the server that aren't master
+        $Databases = Get-AzSqlDatabase -ServerName $SqlServer.Name -ResourceGroupName $SqlServer.ResourceGroupName | Where-Object { $_.DatabaseName -ne "master" }
+
+        $DatabaseConfiguration.$Environment.DatabaseResourceIds = @($Databases.ResourceId)
+        Write-Host "    - Adding $($Databases.Count) databases to $Environment failover group"
+    }
+
+    $ENV:DatabaseConfiguration = $DatabaseConfiguration | ConvertTo-Json
+
+    # --- Set Template parameters
+    Write-Host "- Building deployment parameters file"
+    $ParametersFile = [PSCustomObject]@{
+        "`$schema"     = "http://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#"
+        contentVersion = "1.0.0.0"
+        parameters     = @{ }
+    }
+
+    $TemplateParameters = (Get-Content -Path $PSScriptRoot/templates/subscription.template.json -Raw | ConvertFrom-Json).parameters
+    foreach ($Property in $TemplateParameters.PSObject.Properties.Name) {
+        $ParameterEnvironmentVariableName = $TemplateParameters.$Property.metadata.environmentVariable
+        $ParameterEnvironmentVariableType = $TemplateParameters.$Property.type
+
+        if (!$ParameterEnvironmentVariableName -and ($ParameterEnvironmentVariableType -ne "securestring")) {
+            throw "Could not find environment variable for template parameter $Property"
+        }
+
+        $ParameterVariableValue = Get-Item -Path "ENV:$ParameterEnvironmentVariableName" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Value
+
+        if (!$ParameterVariableValue) {
+            if (!$ENV:TF_BUILD) {
+                if (($TemplateParameters.$Property.defaultValue -or ($TemplateParameters.$Property.defaultValue -ge 0)) -and $AcceptDefaults.IsPresent ) {
+                    $ParameterVariableValue = $TemplateParameters.$Property.defaultValue
+                }
+                else {
+                    $ParameterVariableValue = Read-Host -Prompt "   -> [$($ParameterEnvironmentVariableType)] $($ParameterEnvironmentVariableName)"
+                }
+            }
+            elseif ($ParameterEnvironmentVariableType -ne "securestring") {
+                throw "Could not find environment variable value for template parameter $Property"
+            }
+        }
+
+        switch ($ParameterEnvironmentVariableType) {
+            'array' {
+                $ParameterVariableValue = [String[]]($ParameterVariableValue | ConvertFrom-Json)
+                break
+            }
+            'int' {
+                $ParameterVariableValue = [Int]$ParameterVariableValue
+                break
+            }
+            'object' {
+                $ParameterVariableValue = $ParameterVariableValue | ConvertFrom-Json
+                break
+            }
+            "default" {
+                Write-Warning -Message "Unknown type $ParameterEnvironmentVariableType"
+            }
+        }
+
+        $ParametersFile.parameters.Add($Property, @{ value = $ParameterVariableValue })
+    }
+
+    $null = Set-Content -Path $TemplateParametersFilePath -Value ([Regex]::Unescape(($ParametersFile | ConvertTo-Json -Depth 10))) -Force
+    Write-Host "- Parameter file content saved to $TemplateParametersFilePath"
+
+    if (!$ENV:TF_BUILD ) {
+        Write-Host "- Deploying $TemplateFilePath"
+        $DeploymentParameters = @{
+            ResourceGroupName       = $ManagementResourceGroupName
+            TemplateParameterFile   = $TemplateParametersFilePath
+            TemplateFile            = $TemplateFilePath
+            DeploymentDebugLogLevel = "All"
+        }
+        New-AzResourceGroupDeployment @DeploymentParameters
+    }
+}
+catch {
+    $PSCmdlet.ThrowTerminatingError($_)
+}
---
+++ README.md
@@ -0,0 +1,51 @@
+# Apprenticeship Service Shared Infrastructure
+
+The templates hosted in this repository facilitate the deployment of the shared infrastructure for the Apprenticeships Service.
+
+The deployment consists of two layers.
+
+### Subscription layer
+The subscription layer owns resources that are shared horizontally across a subscription and are used for management purposes. For example, Log Analytics, Azure Automation, KeyVault, Storage, Alerting, Dashboards etc.
+
+**Note**: A subscription can contain one or more environments.
+
+### Environment layer
+The environment layer owns resources that are shared vertically across an environment and are typically used to provide a platform for other independent applications. For example; App Service Plans, Virtual Networks, SQL Servers, ServiceBus etc.
+
+## External dependencies
+There is a third layer that is not deployed by these templates. This is the application layer. Deployment templates for applications within this layer are typically stored with the [application code](https://github.com/SkillsFundingAgency/das-reservations/tree/master/azure) as they will share the same lifecycle. These applications will often depend on infrastructure deployed by templates in this repository.
+
+Both shared and application deployments consume templates from the [platform building blocks](https://github.com/SkillsFundingAgency/das-platform-building-blocks) repository.
+
+## Logical view
+The diagram below is a logical representation of the deployment template structure.
+
+![ApprenticeshipsSharedInfrastructure](images/ApprenticeshipsSharedInfrastructure.png)
+
+## Deployment
+
+### Azure DevOps deployments
+This is the primary method used to deploy the infrastructure. Configuration is stored securely either in the build definition or variable groups and versioned artifacts are used when deploying.
+
+### Local deployment
+To deploy from your local machine run the script below. If matching environment variables are not found, you will be prompted for the values.
+
+**Note**: This deployment method should ***only*** be used for testing purposes.
+
+``` PowerShell
+.\Initialize-SharedInfrastructureDeployment.ps1
+```
+
+If you want to set environment variables, the names expected can be found in the **metadata** object for each parameter in [subscription.template.json](templates/subscription.json).
+
+For example:
+
+``` Javascript
+"resourceEnvironmentName": {
+    "type": "string",
+    "metadata": {
+        "description": "Base environment name. E.g. DEV. PP, PRD, MO. ",
+        "environmentVariable": "resourceEnvironmentName"
+    }
+}
+```
