475de6792f5925951db8c10749e0c46bf1b65f8d
Craig Gumbley <craiggumbley@gmail.com>
2019-06-23 21:41:53 +0100

    Rework logic

--- modules/FailoverGroupBuilder.psm1
+++ modules/FailoverGroupBuilder.psm1
@@ -3,35 +3,29 @@ class FailoverGroupBuilder {
     FailoverGroupBuilder() {
     }
 
-    [string] CreateFailoverGroupConfig([String]$EnvironmentNames) {
+    [string] CreateFailoverGroupConfig([String[]]$EnvironmentNames) {
 
         [Hashtable]$DatabaseConfiguration = @{ }
 
         Write-Host "- Setting up failover group config ->"
 
         $EnvironmentNames | ForEach-Object {
-            Write-Host "    -  Environment $_"
-            Write-Host $DatabaseConfiguration
-
-            $DatabaseConfiguration.Add(
+            $null = $DatabaseConfiguration.Add(
                 $_, @{"DatabaseResourceIds" = @() }
             )
 
-            Write-Host $DatabaseConfiguration
-
             # --- Get all shared SQL Servers in environment
             $SqlServerResources = @(Get-AzResource -Name "das-$($_.ToLower())-shared-sql*" -ResourceType "Microsoft.Sql/servers")
-
             # --- If there is more than one then find the primary
             if ($SqlServerResources.Count -gt 1) {
-                $SqlServer = $SqlServerResources | Where-Object {
+                $SqlServerResources | Where-Object {
                     $FailoverGroup = Get-AzSqlDatabaseFailoverGroup -ServerName $_.Name -ResourceGroupName $_.ResourceGroupName
                     if ($FailoverGroup -and $FailoverGroup.ReplicationRole -eq "Primary") {
-                        return $_
+                        $SqlServer = $_
                     }
                 }
             }
-            elseif ($SqlServerResources -eq 1) {
+            elseif ($SqlServerResources.Name.Count -eq 1) {
                 # --- If there is only one, use that
                 $SqlServer = $SqlServerResources[0]
             }
