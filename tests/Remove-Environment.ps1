85fd0bfbbcf0c346d59c13803ea292c722420da6
Craig Gumbley <craiggumbley@gmail.com>
2019-07-10 10:46:57 +0100

    Add cleanup script (#12)

---
+++ tests/Remove-Environment.ps1
@@ -0,0 +1,21 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $false)]
+    [String]$SubscriptionAbbreviation = "DTA",
+    [Parameter(Mandatory = $false)]
+    [String[]]$EnvironmentNames = "DTA"
+)
+
+$ManagementResourceGroupName = "das-$($SubscriptionAbbreviation)-mgmt-rg"
+$ResourceGroupList = [System.Collections.ArrayList]::new(@($ManagementResourceGroupName.ToLower()))
+$ResourceGroupList.AddRange(@($EnvironmentNames | ForEach-Object { "das-$($_)-shared-rg".ToLower() }))
+
+Write-Host "Cleaning up $($ResourceGroupList.Count) resource group(s)"
+$ResourceGroupList | ForEach-Object {
+    try {
+        Write-Host "    -> $_"
+        $null = Remove-AzResourceGroup -Name $_ -Force
+    } catch {
+        Write-Warning -Message "Failed to cleanup resource group $_"
+    }
+}
