0cf5e82d243cb335aa15e537cba419d13cfa4b07
jack-education <jack>
2020-06-25 13:19:17 +0100

    Added apim-policy folder to Copy task in azure-pipelines.yml

--- apim-policy/Set-ApimPolicy.ps1
+++ apim-policy/Set-ApimPolicy.ps1
@@ -1,13 +1,6 @@
-[CmdletBinding()]
-Param(
-    [Parameter(Mandatory = $false)]
-    [ValidateSet("DTA", "AT", "TEST", "TEST2", "DEMO", "PP", "PRD", "MO")]
-    [String[]]$EnvironmentNames
-)
-
 $EnvironmentNames | ForEach-Object {
-    $ApimContext = New-AzApiManagementContext -ResourceGroupName "das-$($_)-apim-rg".ToLower() -ServiceName "das-$($_)-shared-apim".ToLower()
     Write-Host "Environment: $($_)"
+    $ApimContext = New-AzApiManagementContext -ResourceGroupName "das-$($_)-apim-rg".ToLower() -ServiceName "das-$($_)-shared-apim".ToLower()
     Get-AzApiManagementPolicy -Context $ApimContext
     #Set-AzApiManagementPolicy -Context $ApimContext -PolicyFilePath "./apim-policy/apim-policy.xml"
 }
