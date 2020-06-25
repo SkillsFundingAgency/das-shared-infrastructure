3217d54d8a8168a8f450ad5b0ec681c3f7efb9e3
jack-education <jack>
2020-06-25 19:56:35 +0100

    Added Write-Output

--- apim-policy/Set-ApimPolicy.ps1
+++ apim-policy/Set-ApimPolicy.ps1
@@ -5,8 +5,9 @@ Param(
     [String[]]$EnvironmentNames
 )
 $EnvironmentNames | ForEach-Object {
-    Write-Host "Environment: $($_)"
+    Write-Output "Environment: $($_)"
     $ApimContext = New-AzApiManagementContext -ResourceGroupName "das-$($_)-apim-rg".ToLower() -ServiceName "das-$($_)-shared-apim".ToLower()
     Set-AzApiManagementPolicy -Context $ApimContext -PolicyFilePath "$PSScriptRoot/apim-policy.xml"
+    Write-Output "APIM policy:"
     Get-AzApiManagementPolicy -Context $ApimContext
 }
