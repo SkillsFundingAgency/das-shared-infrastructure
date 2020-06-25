b295490acda1f95fb7e82baa5cfb0bbafb845b4d
jack-education <jack>
2020-06-25 19:07:12 +0100

    Removed reference to environment variable

--- apim-policy/Set-ApimPolicy.ps1
+++ apim-policy/Set-ApimPolicy.ps1
@@ -1,4 +1,10 @@
-$ENV:EnvironmentNames | ForEach-Object {
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("DTA", "AT", "TEST", "TEST2", "DEMO", "PP", "PRD", "MO")]
+    [String[]]$EnvironmentNames
+)
+$EnvironmentNames | ForEach-Object {
     Write-Host "Environment: $($_)"
     $ApimContext = New-AzApiManagementContext -ResourceGroupName "das-$($_)-apim-rg".ToLower() -ServiceName "das-$($_)-shared-apim".ToLower()
     Get-AzApiManagementPolicy -Context $ApimContext
