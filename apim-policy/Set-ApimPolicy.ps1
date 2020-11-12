6b30d4789b55c34d399a1bacbfbdf1bab73a02f1
Mohamad Adam <accadam230594@outlook.com>
2020-11-12 14:48:03 +0000

    updated policy doc

--- apim-policy/Set-ApimPolicy.ps1
+++ apim-policy/Set-ApimPolicy.ps1
@@ -2,12 +2,27 @@
 Param(
     [Parameter(Mandatory = $true)]
     [ValidateSet("DTA", "AT", "TEST", "TEST2", "DEMO", "PP", "PRD", "MO")]
-    [String[]]$EnvironmentNames
+    [String[]]$EnvironmentNames,
+    [Parameter(Mandatory = $true)]
+    [String]$CustomDomainHostedZone
+
 )
+
+$PolicyFilePath = "$PSScriptRoot/apim-policy.xml"
 $EnvironmentNames | ForEach-Object {
-    Write-Output "Environment: $($_)"
+    $PortalDomain = "portal." + $CustomDomainHostedZone
+    if ($_ -ne "PRD") {
+        $PortalDomain = "https://" + $_.ToLower() + "-" + $PortalDomain
+    }
+    else {
+        $PortalDomain = "https://" + $PortalDomain
+    }
+
+    [xml]$XmlData = (Get-Content $PolicyFilePath)
+    $XmlData.policies.inbound.cors.'allowed-origins'.origin = $PortalDomain
+    $XmlData.Save($PolicyFilePath)
     $ApimContext = New-AzApiManagementContext -ResourceGroupName "das-$($_)-apim-rg".ToLower() -ServiceName "das-$($_)-shared-apim".ToLower()
-    Set-AzApiManagementPolicy -Context $ApimContext -PolicyFilePath "$PSScriptRoot/apim-policy.xml"
+    Set-AzApiManagementPolicy -Context $ApimContext -PolicyFilePath $PolicyFilePath
     Write-Output "APIM policy:"
     Get-AzApiManagementPolicy -Context $ApimContext
 }
