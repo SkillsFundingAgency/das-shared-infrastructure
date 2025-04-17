3e0535cfe0ca225579c492b990fa9df3d52807ff
Rohon Debray <95615458+debrr07@users.noreply.github.com>
2025-04-17 13:16:37 +0100

    Updated UnitTest.Helpers.psm1

--- tests/modules/UnitTest.Helpers.psm1
+++ tests/modules/UnitTest.Helpers.psm1
@@ -33,7 +33,7 @@ function Set-MockEnvironment {
     $ENV:databasesToIncludeIn100PercentAlert = "das-prd-example-db"
     $ENV:gatewaySubnetName = "subnet"
     $ENV:products = "['faa']"
-    $ENV:deployPrivateLinkedScopedResource = false
+    $ENV:deployPrivateLinkedScopedResource = "false"
     $ENV:routeTableRoutes = "{}"
 }
 
@@ -68,6 +68,7 @@ function Clear-MockEnvironment {
         "ENV:logAnalyticsWorkspaceSku",
         "ENV:logAnalyticsWorkspaceDataRetentionDays",
         "ENV:products",
+        "ENV:deployPrivateLinkedScopedResource",
         "ENV:routeTableRoutes",
         "ENV:actionGroupName",
         "ENV:actionGroupResourceGroupName"
